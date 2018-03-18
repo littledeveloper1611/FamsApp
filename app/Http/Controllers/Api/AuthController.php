11c9a9339a8edfdbc6c9f4aa5bc78f9c7fbc083e
nesdejavu <nesdejavu@gmail.com>
2018-03-18 22:00:20 +0700

    Intial commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Laravel\Passport\Client;
+
+class AuthController extends Controller
+{
+	private $client;
+	public function __construct(){
+		$this->client = Client::find(1);
+	}
+
+    public function postLogin(Request $request){
+    	$this->validate($request,[
+    		'username' => 'required',
+    		'password' => 'required'
+    	]);
+
+
+    }
+}
